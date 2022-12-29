0ec12e1fd31e692a108f5498e21dcf6cd1ac7b21
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-29 23:48:26 +0600

    project is set up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <button class="btn w-64 rounded-full">Button</button>
     </div>
   );
 }
